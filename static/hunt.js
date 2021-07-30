00c8de2f3d4b249d56410ef34211ccf561f8e4e9
qsilver7598 <39454454+qsilver7598@users.noreply.github.com>
2021-07-30 23:28:16 +0900

    working on getting the http requests working

--- static/hunt.js
+++ static/hunt.js
@@ -4,6 +4,74 @@
 
 let map, infoWindow;
 
+// const Url='http://localhost:8080';
+const Url='https://cs467-capstone.uw.r.appspot.com';
+
+
+// jQuery functions for interaction with the database
+$(document).ready(function(){
+  $('#createHunt').click(function(){
+    var hunt = getHuntInfo();
+    if (hunt == 'error'){
+      alert('One or more of the fields are empty. Please try again.');
+    }
+    else{
+      $.ajax({
+        url: Url + '/hunts',
+        type: 'POST',
+        data: getHuntInfo(),
+        contentType: 'application/json',
+        dataType: 'json',
+        headers: {
+          Authorization: 'Bearer ' + token
+        },
+        success: function(response){
+          $('#create-hunt-form').trigger('reset');
+        },
+        error: function(){
+          alert('There was an error with your request.')
+          window.location.href = '/'
+        }
+      })
+    }
+  })
+  $('#b2').click(function(){
+    $.ajax({
+      url: Url + '/hunts',
+      type: 'GET',
+      success: function(response){
+        var returnedData = JSON.parse(response);
+        createHuntList(returnedData['hunts']);
+        
+      }
+    })
+  })
+})
+
+
+function getHuntInfo(){
+  var hunt = {
+    name: $("#hunt-title").val(),
+    theme: $("#hunt-theme").val(),
+    clues: [],
+    treasures: []
+  }
+  if (hunt['name'] == '' || hunt['theme'] == ''){
+    return 'error';
+  }
+  return JSON.stringify(hunt);
+}
+
+
+function createHuntList(hunts){
+  $.each(hunts, function(key, val){
+    var radioBtn = $('<input type="radio" id=' + key + ' />');
+    var label = $('<label id=huntName>' + val['name'] + '</label>' + '<br><br>');
+    radioBtn.appendTo('#find-hunt-radio-box');
+    label.appendTo('#find-hunt-radio-box');
+  })
+}
+
 function initMap() {
     
   map = new google.maps.Map(document.getElementById("map"), {
@@ -91,20 +159,23 @@ function toggle_C2() {
 }
 
 function toggleButton2() {
+  // wait for get requests
+  setTimeout(function(){
     var x = document.getElementById("c1");
     var y = document.getElementById("c3");
-
+  
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
-
+  
     if (y.style.display === "none") {
         y.style.display = "block";
     } else {
         y.style.display = "none";
     }
+  }, 1500);
 }
 
 
@@ -120,52 +191,61 @@ function addClue(){
 
 }
 
-function submitHunt(){
-    toggleButton1()
+// function submitHunt(){
+//     toggleButton1()
     
-}
-// this function was adapted from https://gist.github.com/prof3ssorSt3v3/52ebd432bb7b8a155985a2f82509541d
-//outer array likely not needed for our purposes
+// }
+// // this function was adapted from https://gist.github.com/prof3ssorSt3v3/52ebd432bb7b8a155985a2f82509541d
+// //outer array likely not needed for our purposes
 
-let hunt_list = [];
-const huntCreate = (ev)=>{
-  //ev.preventDefault();  //to stop the form submitting
-  let hunt = {
-      id: Date.now(),
-      username: document.getElementById('user-name').value,
-      title: document.getElementById('hunt-title').value,
-      theme: document.getElementById('hunt-theme').value,
-      treasure: document.getElementById('treasure').value,
-      clue1: document.getElementById('clue1').value,
-      clue2: document.getElementById('clue2').value,
-      clue3: document.getElementById('clue3').value,
-      clue4: document.getElementById('clue4').value
+// let hunt_list = [];
+// const huntCreate = (ev)=>{
+//   //ev.preventDefault();  //to stop the form submitting
+//   let hunt = {
+//       title: document.getElementById('hunt-title').value,
+//       theme: document.getElementById('hunt-theme').value,
+//       clues: [],
+//       treasures: []
+//       // treasure: document.getElementById('treasure').value,
+//       // clue1: document.getElementById('clue1').value,
+//       // clue2: document.getElementById('clue2').value,
+//       // clue3: document.getElementById('clue3').value,
+//       // clue4: document.getElementById('clue4').value
 
-      //theme: document.querySelector('#radio-box input').value
-  }
-  hunt_list.push(hunt);
-  // to clear the form for the next entries
-  document.querySelector('form').reset();
+//       //theme: document.querySelector('#radio-box input').value
+//   }
+//   hunt_list.push(hunt);
+//   // to clear the form for the next entries
+//   document.querySelector('form').reset();
 
-  //for display purposes only
-  console.warn('added' , {hunt} );
-  console.log(hunt)
-  console.log(hunt.clue1)
-  let pre = document.querySelector('#msg pre');
-  pre.textContent = '\n' + JSON.stringify(hunt_list, '\t', 2);
-  setTimeout(alert("paused"),4000);
+//   //for display purposes only
+//   console.warn('added' , {hunt} );
+//   console.log(hunt)
+//   let pre = document.querySelector('#msg pre');
+//   pre.textContent = '\n' + JSON.stringify(hunt_list, '\t', 2);
+//   setTimeout(alert("paused"),4000);
 
-  //send to server
-  const myHuntJSON = JSON.stringify(hunt);
-  window.location = "https://cs467-capstone.uw.r.appspot.php?x=" + myHuntJSON;//or whatever the correct address is
-}
+//   //send to server
+//   var request = new XMLHttpRequest();
+//   request.open("POST", "http://localhost:8080/hunts", true);
+//   request.setRequestHeader('Authorization', 'Bearer ' + token);
+//   request.setRequestHeader('Content-Type', 'application/json');
+//   console.log(request)
+//   request.send(JSON.stringify(hunt));
 
-function playHunt(){
+//   request.onreadystatechange = function () {
+//     if (request.readyState === 4) {
+//       alert(request.responstText);
+//     }
+//   }
+//   // const myHuntJSON = JSON.stringify(hunt);
+//   // window.location = "https://cs467-capstone.uw.r.appspot.com/hunts" + myHuntJSON;//or whatever the correct address is
+// }
+
+// function playHunt(){
   
-}
+// }
 
-
-document.addEventListener('DOMContentLoaded', ()=>{
-document.getElementById('submit').addEventListener('click', huntCreate);
-});
-
+// document.addEventListener('DOMContentLoaded', ()=>{
+// document.getElementById('submit').addEventListener('click', huntCreate);
+// });
