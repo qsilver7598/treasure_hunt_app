db14560f5c06187a1e37e27dc88f28bcd29f2a1c
MarcellaLarissa <56004783+MarcellaLarissa@users.noreply.github.com>
2021-08-04 12:31:38 -0700

    v17

--- static/hunt.js
+++ static/hunt.js
@@ -7,7 +7,7 @@ let map, infoWindow;
 // const Url='http://localhost:8080';
 const Url='https://cs467-capstone.uw.r.appspot.com';
 
-console.log('version 16')
+console.log('version 17')
 // jQuery functions for interaction with the database
 // CREATE HUNT
 $(document).ready(function(){
@@ -67,7 +67,7 @@ $(document).ready(function(){
           Authorization: 'Bearer ' + token
         },
         success: function(response){
-          //$('#create-hunt-form').trigger('reset');
+          $('#create-hunt-form').trigger('reset');
           alert(hunt.name, 'clue was added successfully.')//not resetting form on submission so we can still grab values without having to set globals
         },
         error: function(){
@@ -97,7 +97,7 @@ $(document).ready(function(){
           Authorization: 'Bearer ' + token
         },
         success: function(response){
-          //$('#create-hunt-form').trigger('reset');
+          $('#create-hunt-form').trigger('reset');
           alert(hunt.name, 'treasure was added successfully.')//not resetting form on submission so we can still grab values without having to set globals
         },
         error: function(){
